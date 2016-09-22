9fd490373e73a217206e6fa22a626b43c1affef3
danielyoshizawa <yoshidanielcwb@gmail.com>
2016-09-22 15:44:34 -0300

    Integrating Slick2d

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,8 +1,23 @@
 package Game;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 public class Main {
 
     public static void main(String [] args) {
-        System.out.print("Hello Box2d");
+        World world = new World("Box2d Test Game");
+
+        try
+        {
+            AppGameContainer app = new AppGameContainer(world);
+            app.setDisplayMode(500, 400, false);
+            app.start();
+
+        }
+        catch (SlickException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
